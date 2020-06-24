a324e58111600e99e402845f04dd07092a51c7e7
Philip Kueng <philip.kueng@nzz.ch>
2020-06-24 15:50:04 +0200

    adjust legend width

--- views/ChoroplethLegend.svelte
+++ views/ChoroplethLegend.svelte
@@ -37,57 +37,59 @@
     </div>
   {:else if legendData.type === 'numerical'}
     <!-- display bucket legend -->
-    <div class="q-choropleth-legend-container">
-      {#if hasSingleValueBucket(legendData)}
-        <svg class="q-choropleth-single-value-bucket">
+    <div style="display: flex; justify-content: center;">
+      <div class="q-choropleth-legend-container">
+        {#if hasSingleValueBucket(legendData)}
+          <svg class="q-choropleth-single-value-bucket">
+            <g>
+              <rect
+                class="q-choropleth-legend-bucket {legendData.buckets[0].color.colorClass}"
+                style="fill: {legendData.buckets[0].color.customColor}"
+                width={singleValueBucketWidth}
+                height={legendBarHeight}
+                x="0"
+                y="20" />
+            </g>
+          </svg>
+        {/if}
+        <div style="width: 100%; display: flex; justify-content: space-between;">
+          <span style="margin-left: 2px" class="s-font-note">{legendData.minValue}</span>
+          <span style="margin-right: 2px" class="s-font-note">{legendData.maxValue}</span>
+        </div>
+        <svg class="q-choropleth-legend">
+          <g>
+            {#each legendData.buckets as bucket, index}
+              {#if !(hasSingleValueBucket(legendData) && index === 0)}
+                <rect
+                  class="q-choropleth-legend-bucket {bucket.color.colorClass}"
+                  style="fill: {bucket.color.customColor}"
+                  width="{getAspectWidth(legendData, bucket)}%"
+                  height={legendBarHeight}
+                  x="{getAspectXValue(legendData, bucket)}%"
+                  y={legendBarHeight} />
+              {/if}
+            {/each}
+          </g>
+          <g>          
+            <rect
+                  class="s-color-gray-9"
+                  style="fill: currentColor;"
+                  width="0.5px"
+                  height={legendBarHeight * 1.8}
+                  x="0%"
+                  y="3" />
+          </g>
           <g>
             <rect
-              class="q-choropleth-legend-bucket {legendData.buckets[0].color.colorClass}"
-              style="fill: {legendData.buckets[0].color.customColor}"
-              width={singleValueBucketWidth}
-              height={legendBarHeight}
-              x="0"
-              y="20" />
+                  class="s-color-gray-9"
+                  style="fill: currentColor;"
+                  width="0.5px"
+                  height={legendBarHeight * 1.8}
+                  x="99.92%"
+                  y="3" />
           </g>
         </svg>
-      {/if}
-      <div style="width: 100%; display: flex; justify-content: space-between;">
-        <span style="margin-left: 2px" class="s-font-note">{legendData.minValue}</span>
-        <span style="margin-right: 2px" class="s-font-note">{legendData.maxValue}</span>
       </div>
-      <svg class="q-choropleth-legend">
-        <g>
-          {#each legendData.buckets as bucket, index}
-            {#if !(hasSingleValueBucket(legendData) && index === 0)}
-              <rect
-                class="q-choropleth-legend-bucket {bucket.color.colorClass}"
-                style="fill: {bucket.color.customColor}"
-                width="{getAspectWidth(legendData, bucket)}%"
-                height={legendBarHeight}
-                x="{getAspectXValue(legendData, bucket)}%"
-                y={legendBarHeight} />
-            {/if}
-          {/each}
-        </g>
-        <g>          
-          <rect
-                class="s-color-gray-9"
-                style="fill: currentColor;"
-                width="0.5px"
-                height={legendBarHeight * 1.8}
-                x="0%"
-                y="3" />
-        </g>
-        <g>
-          <rect
-                class="s-color-gray-9"
-                style="fill: currentColor;"
-                width="0.5px"
-                height={legendBarHeight * 1.8}
-                x="99.92%"
-                y="3" />
-        </g>
-      </svg>
     </div>
   {/if}
 {/if}
