8167ee422945f1676502569757b362b2a0075d62
fuenkchen <sharon.funke@nzz.ch>
2020-09-13 12:57:21 +0200

    Add responsive svg for IE support

---
+++ views/Hexagon/ResponsiveSvg.svelte
@@ -0,0 +1,10 @@
+<script>
+  // Wrapper for a responsive SVG that also works in Internet Explorer
+  export let aspectRatio;
+</script>
+
+<div
+  class="q-choropleth-svg-container"
+  style="padding-top: {100 / aspectRatio}%">
+  <slot />
+</div>
