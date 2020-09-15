09c7f6587ddd90c647ff15ef8e064910b97b689b
fuenkchen <sharon.funke@nzz.ch>
2020-09-15 17:52:55 +0200

    Cleanup: remove some stuff, add comments for better understanding

--- views/Hexagon/HexagonCHCantonsMap.svelte
+++ views/Hexagon/HexagonCHCantonsMap.svelte
@@ -21,6 +21,7 @@
 
   // See https://www.redblobgames.com/grids/hexagons/#size-and-spacing
   const rowHeight = (cellHeight * 3) / 4;
+
   const hexagons = getHexagons(contentWidth);
   const svgSize = getSvgSize(hexagons);
 
