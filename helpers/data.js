724e1d745a51de0c8d69ec1d40fabff8b10729b8
Philip Kueng <philip.kueng@nzz.ch>
2020-10-28 10:33:00 +0100

    use object-function to get categories-length

--- helpers/data.js
+++ helpers/data.js
@@ -6,11 +6,7 @@ function getDataWithoutHeaderRow(data) {
 }
 
 function getUniqueCategoriesCount(data) {
-  return data
-    .map((row) => {
-      return row[1];
-    })
-    .filter((value) => value !== null);
+  return getUniqueCategoriesObject(data).categories.length;
 }
 
 function getUniqueCategoriesObject(data) {
