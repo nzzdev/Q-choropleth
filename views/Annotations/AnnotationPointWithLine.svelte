411be22d05b5b917b98949701ad80c57af87e71b
Nicolas Staub <nicolas.staub@nzz.ch>
2021-01-21 14:23:18 +0100

    round the coordinates

--- views/Annotations/AnnotationPointWithLine.svelte
+++ views/Annotations/AnnotationPointWithLine.svelte
@@ -1,13 +1,14 @@
 <script>
   import AnnotationPoint from "./AnnotationPoint.svelte";
+  import { round } from "../helpers/data.js";
 
   // General props
   export let id = 0;
   export let radius = 0;
   export let coordinates = [];
 
-  let x = coordinates.x;
-  let y = coordinates.y;
+  let x = round(coordinates.x);
+  let y = round(coordinates.y);
 
   // AnnotationPoint props
   export let strokeColor = "black";
@@ -20,29 +21,31 @@
   // Line props
   export let strokeDashArray;
 
-  let lineX1 = coordinates.lineX1;
-  let lineY1 = coordinates.lineY1;
-  let lineX2 = coordinates.lineX2;
-  let lineY2 = coordinates.lineY2;
+  let lineX1 = round(coordinates.lineX1);
+  let lineY1 = round(coordinates.lineY1);
+  let lineX2 = round(coordinates.lineX2);
+  let lineY2 = round(coordinates.lineY2);
 </script>
 
-<AnnotationPoint
-  {id}
-  {radius}
-  {x}
-  {y}
-  {strokeColor}
-  {strokeWidth}
-  {colorClass}
-  {fillColor}
-  {textColor}
-  {fontSize} />
-<line
-  x1={lineX1}
-  y1={lineY1}
-  x2={lineX2}
-  y2={lineY2}
-  stroke={strokeColor}
-  stroke-width={strokeWidth}
-  stroke-dasharray={strokeDashArray}>
-</line>
+<g class="q-choropleth-annotation">
+  <AnnotationPoint
+    {id}
+    {radius}
+    {x}
+    {y}
+    {strokeColor}
+    {strokeWidth}
+    {colorClass}
+    {fillColor}
+    {textColor}
+    {fontSize} />
+  <line
+    x1={lineX1}
+    y1={lineY1}
+    x2={lineX2}
+    y2={lineY2}
+    stroke={strokeColor}
+    stroke-width={strokeWidth}
+    stroke-dasharray={strokeDashArray}>
+  </line>
+</g>
