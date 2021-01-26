360ba635b4d37e498de6755246fa777efeab74d8
Nicolas Staub <nicolas.staub@nzz.ch>
2021-01-26 15:32:02 +0100

    fix missing indent issue

--- views/Annotations/AnnotationsLegend.svelte
+++ views/Annotations/AnnotationsLegend.svelte
@@ -5,8 +5,6 @@
   export let annotations = [];
 
   let radius = 8;
-  let svgWidth = (2 * radius + 2);
-  let svgHeight = (2 * radius + 2);
   let x = (radius + 1);
   let y = (radius + 1);
   
@@ -19,23 +17,25 @@
   export let fontSize = "11px";
 </script>
 
-<div class="q-choropleth-annotations-legend s-font-note" style="display: flex; flex-direction: column; margin: 8px 0 0 0;">
+<div class="q-choropleth-legend-annotations s-font-note" style="display: flex; flex-direction: column;">
   {#each annotations as { id, name }}
-    <div class="q-choropleth-annotation-legend" style="margin: 0 0 4px 0;">
-      <svg width={svgWidth} height={svgHeight} style="vertical-align: sub;">
-        <AnnotationPoint
-          {id}
-          {radius}
-          {x}
-          {y}
-          {strokeColor}
-          {strokeWidth}
-          {colorClass}
-          {fillColor}
-          {textColor}
-          {fontSize} />
-      </svg>
-      <span>{name}</span>
+    <div class="q-choropleth-legend-annotation" style="display: flex; flex-direction: row; margin-top: 4px;">
+      <div style="height: 18px; width: 18px; margin-right: 4px;">
+        <svg width=18 height=18 viewBox="0 0 18 18">
+          <AnnotationPoint
+            {id}
+            {radius}
+            {x}
+            {y}
+            {strokeColor}
+            {strokeWidth}
+            {colorClass}
+            {fillColor}
+            {textColor}
+            {fontSize} />
+        </svg>
+      </div>
+      <div>{name}</div>
     </div>
   {/each}
 </div>
