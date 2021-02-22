7296c8cf4bc9c43f4d7d390fd06b41bd5490ad73
fgervasi <fgervasi12@gmail.ch>
2021-02-22 17:29:54 +0100

    Definition sophie-q classes

--- views/Annotations/AnnotationsLegend.svelte
+++ views/Annotations/AnnotationsLegend.svelte
@@ -1,17 +1,17 @@
 <script>
   import AnnotationPoint from "./AnnotationPoint.svelte";
-  
+
   export let annotations = [];
   export let annotationRadius = 8;
-  
+
   let x = (annotationRadius + 1);
   let y = (annotationRadius + 1);
 </script>
 
-<div class="q-choropleth-legend-annotations s-font-note" style="display: flex; flex-direction: column; margin-bottom: 8px;">
+<div class="s-q-item__legend-annotations s-font-note">
   {#each annotations as { id, annotation }}
-    <div class="q-choropleth-legend-annotation" style="display: flex; flex-direction: row; margin-top: 4px;">
-      <div style="height: 18px; width: 18px; margin-right: 4px;">
+    <div class="s-q-item__legend-annotation">
+      <div class="s-q-item__legend-annotation__icon">
         <svg width=18 height=18 viewBox="0 0 18 18">
           <AnnotationPoint
             {id}
@@ -20,7 +20,7 @@
             {y} />
         </svg>
       </div>
-      <div style="display: flex; align-items: center;">{annotation}</div>
+      <div class="s-q-item__legend-annotation__label">{annotation}</div>
     </div>
   {/each}
 </div>
