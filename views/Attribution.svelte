46b28bf26e031319a42a24182c58922c24236ce8
Manuel Roth <manuel.roth@nzz.ch>
2021-04-30 10:36:54 +0200

    Improve map attribution

--- views/Attribution.svelte
+++ views/Attribution.svelte
@@ -3,10 +3,12 @@
   export let isStatic;
 </script>
 
-<div class="q-choropleth-attribution s-font-note-s">
-  {#if isStatic}
-    Kartengrundlage: {source.label}
-  {:else}
-    Kartengrundlage: <a href="{source.url}" target="_blank" rel="noopener noreferrer">{source.label}</a>
-  {/if}
+<div class="q-choropleth-attribution s-font-note-s s-font-note-s--light">
+  <span class="s-color-gray-6">
+    {#if isStatic}
+      Kartengrundlage: {source.label}
+    {:else}
+      Kartengrundlage: <a href="{source.url}" target="_blank" rel="noopener noreferrer">{source.label}</a>
+    {/if}
+  </span>
 </div>
