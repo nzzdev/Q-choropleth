88be0bcf880eba45869dae70f7d1301890dc6e46
Manuel Roth <manuelroth@hotmail.ch>
2021-05-19 18:50:43 +0200

    Improve init of graphic

--- views/Choropleth.svelte
+++ views/Choropleth.svelte
@@ -37,52 +37,54 @@
   }
 </script>
 
-<div bind:this={container}>
-  {#if !(legendData.type === 'categorical' && valuesOnMap)}
-    <ChoroplethLegend
-      {legendData}
-      {formattingOptions}
-      {contentWidth}
-      {isStatic} />
-  {/if}
-  {#if item.baseMap.includes('hexagon')}
-    <HexagonMap
-      {dataMapping}
-      entityType={item.entityType}
-      {valuesOnMap}
-      {legendData}
-      {baseMap}
-      {contentWidth}
-      {formattingOptions}
-      {maxHeight}
-      {annotations}
-      {annotationRadius} />
-  {/if}
-  {#if item.baseMap.includes('geographic')}
-    <GeographicMap
-      {dataMapping}
-      entityType={item.entityType}
-      {legendData}
-      {baseMap}
-      {contentWidth}
-      {maxHeight}
-      {annotations}
-      {annotationRadius} />
-  {/if}
-  {#if annotations && annotations.length > 0}
-    <AnnotationsLegend
-      {annotations}
-      {annotationRadius} />
-  {/if}
-  {#if baseMap.source}
-    <Attribution source={baseMap.source} {isStatic}/>
-  {/if}
-  {#if legendData.type === 'numerical'}
-    <MethodBox
-      {legendData}
-      {formattingOptions}
-      {isStatic}
-      methodBoxText={methodBox.text}
-      methodBoxArticle={methodBox.article} />
-  {/if}
-</div>
+{#if contentWidth}
+  <div bind:this={container}>
+    {#if !(legendData.type === 'categorical' && valuesOnMap)}
+      <ChoroplethLegend
+        {legendData}
+        {formattingOptions}
+        {contentWidth}
+        {isStatic} />
+    {/if}
+    {#if item.baseMap.includes('hexagon')}
+      <HexagonMap
+        {dataMapping}
+        entityType={item.entityType}
+        {valuesOnMap}
+        {legendData}
+        {baseMap}
+        {contentWidth}
+        {formattingOptions}
+        {maxHeight}
+        {annotations}
+        {annotationRadius} />
+    {/if}
+    {#if item.baseMap.includes('geographic')}
+      <GeographicMap
+        {dataMapping}
+        entityType={item.entityType}
+        {legendData}
+        {baseMap}
+        {contentWidth}
+        {maxHeight}
+        {annotations}
+        {annotationRadius} />
+    {/if}
+    {#if annotations && annotations.length > 0}
+      <AnnotationsLegend
+        {annotations}
+        {annotationRadius} />
+    {/if}
+    {#if baseMap.source}
+      <Attribution source={baseMap.source} {isStatic}/>
+    {/if}
+    {#if legendData.type === 'numerical'}
+      <MethodBox
+        {legendData}
+        {formattingOptions}
+        {isStatic}
+        methodBoxText={methodBox.text}
+        methodBoxArticle={methodBox.article} />
+    {/if}
+  </div>
+{/if}
