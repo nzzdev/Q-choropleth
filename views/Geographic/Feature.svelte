d2cc976379fb14e27911493e1febb4548b4a6006
Manuel Roth <manuelroth@hotmail.ch>
2021-11-30 16:21:18 +0100

    (geographic) Null values should be displayed with white fill and gray stroke

--- views/Geographic/Feature.svelte
+++ views/Geographic/Feature.svelte
@@ -1,19 +1,31 @@
 <script>
   export let color;
+  export let value;
   export let path;
   export let hasAnnotation = false;
   export let strokeWidth;
 </script>
 
 <g class={color.colorClass}>
-  <path
-    fill={color.customColor && color.customColor.length > 0
-      ? color.customColor
-      : "currentColor"}
-    stroke="#fff"
-    stroke-width={strokeWidth}
-    d={path}
-  />
+  {#if value !== null && value !== undefined}
+    <path
+      fill={color.customColor && color.customColor.length > 0
+        ? color.customColor
+        : "currentColor"}
+      stroke="#fff"
+      stroke-width={strokeWidth}
+      d={path}
+    />
+  {:else}
+    <path
+      fill="#fff"
+      stroke={color.customColor && color.customColor.length > 0
+        ? color.customColor
+        : "currentColor"}
+      stroke-width={strokeWidth}
+      d={path}
+    />
+  {/if}
   {#if hasAnnotation}
     <path
       class="s-color-gray-9"
