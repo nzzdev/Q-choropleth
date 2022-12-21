453585f6a095eaf1154a20a005df759d6af1b18c
Nicolas Staub <nicolas.staub@nzz.ch>
2022-12-21 17:03:30 +0100

    add fixture data for europe map

--- routes/fixtures/data.js
+++ routes/fixtures/data.js
@@ -3,6 +3,7 @@ const fixtureDataDirectory = "../../resources/fixtures/data";
 // provide every fixture data file present in ../../resources/fixtures/data
 // has to be in sync with files created in build task - see ../../tasks/build.js
 const fixtureData = [
+  require(`${fixtureDataDirectory}/geographic-numeric-map-europe.json`),
   require(`${fixtureDataDirectory}/geographic-categorical-show-bubble-map.json`),
   require(`${fixtureDataDirectory}/geographic-numeric-hide-bubble-map.json`),
   require(`${fixtureDataDirectory}/geographic-numeric-show-bubble-map.json`),
